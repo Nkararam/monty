e824fff23da72d4382d4cc35b994719dccda7910
Nkararam <carolinekararam@gmail.com>
2023-06-22 09:19:19 -0700

    task 5

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+  *nop- nothing
+  *@stack: double pointer to head node of list
+  *@ln: bycode number
+ */
+void nop(stack_t **stack, unsigned int ln)
+{
+(void) ln;
+(void) stack;
+}
