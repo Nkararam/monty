448f7ff0cdaae46051fb477d5949772fa35368cd
Nkararam <carolinekararam@gmail.com>
2023-06-22 13:57:49 -0700

    rotl.c

---
+++ rotl.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+  *rotl- rotates the stack to the top
+  *@stack: pointer to lists for monty stack
+  *@ln: number of line opcode occurs on
+ */
+void rotl(stack_t **stack, unsigned int ln)
+{
+stack_t *temp = *stack, *aux;
+if (*stack == NULL || (*stack)->next == NULL)
+{
+return;
+}
+aux = (*stack)->next;
+aux->prev = NULL;
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
+temp->next = *stack;
+(*stack)->next = NULL;
+(*stack)->prev = temp;
+(*stack) = aux;
+}
