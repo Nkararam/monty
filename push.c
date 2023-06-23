2b53d1c76bb42d8a5ab4eef6345d0ebed9ef1ce2
Nkararam <carolinekararam@gmail.com>
2023-06-22 17:26:46 -0700

    ahahahaah

--- push.c
+++ push.c
@@ -1,30 +1,39 @@
 #include "monty.h"
+
 /**
- * push - add node to the stack
-* @stack: double pointer to head node of list
+ * push - pushes an element to the stack
+ * @stack: double pointer to head node of list
  * @ln: line number of bytecode file
 */
 void push(stack_t **stack, unsigned int ln)
 {
-int n, j = 0, flag = 0;
+	stack_t *new_node;
+	int data;
 
-if (bus.arg)
-}
-if (bus.arg[0] == '-')
-j++;
-for (; bus.arg[j] != '\0'; j++)
-{
-if (bus.arg[j] > 57 || bus.arg[j] < 48)
-flag = 1; }
-if (flag == 1)
-{fprintf(stderr, "L%d: usage: push integer\n", ln);
-exit(EXIT_FAILURE); }}
-else
-{fprintf(stderr, "L%d: usage: push integer\n", ln);
-exit(EXIT_FAILURE); }
-n = atoi(bus.arg);
-if (bus.lifi == 0)
-addnode(stack, n);
-else
-addqueue(stack, n);
+	new_node = malloc(sizeof(size_t));
+	if (!new_node)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!monty.arg || (_isdigit(monty.arg) == -1))
+	{
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", ln);
+		exit(EXIT_FAILURE);
+	}
+	data = atoi(monty.arg);
+	new_node->n = data;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (!(*stack))
+	{
+		(*stack) = new_node;
+		return;
+	}
+	/* if stack is not NULL */
+	new_node->next = (*stack);
+	(*stack)->prev = new_node;
+	(*stack) = new_node;
 }
