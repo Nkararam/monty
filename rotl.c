0853dd522c5d18e1ca9060236abd18eff22c85eb
Nkararam <carolinekararam@gmail.com>
2023-06-23 06:41:44 -0700

    weeeuh

--- rotl.c
+++ rotl.c
@@ -4,7 +4,7 @@
   *@stack: pointer to lists for monty stack
   *@ln: number of line opcode occurs on
  */
-void rotl(stack_t **stack)
+void rotl(stack_t **stack, unsigned int ln)
 {
 stack_t *temp = *stack, *aux;
 if (*stack == NULL || (*stack)->next == NULL)
