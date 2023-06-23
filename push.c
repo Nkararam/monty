cde2420d14e70027bf46a96bed02f8643c9078a5
Nkararam <carolinekararam@gmail.com>
2023-06-23 06:47:52 -0700

    debugging and updating

--- push.c
+++ push.c
@@ -1,39 +1,38 @@
 #include "monty.h"
 
 /**
- * push - pushes an element to the stack
+ * push - adds an element to the stack
  * @stack: double pointer to head node of list
  * @ln: line number of bytecode file
 */
 void push(stack_t **stack,unsigned int ln);
-
 {
-	stack_t *new_node;
-	int data;
+stack_t *new_node;
+int data;
 
-	new_node = malloc(sizeof(size_t));
-	if (!new_node)
-	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+new_node = malloc(sizeof(size_t));
+if (!new_node)
+{
+dprintf(STDERR_FILENO, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
 
-	if (!monty.arg || (_isdigit(monty.arg) == -1))
-	{
-		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", ln);
-		exit(EXIT_FAILURE);
-	}
-	data = atoi(monty.arg);
-	new_node->n = data;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+if (!monty.arg || (_isdigit(monty.arg) == -1))
+{
+dprintf(STDERR_FILENO, "L%u: usage: push integer\n", ln);
+exit(EXIT_FAILURE);
+}
+data = atoi(monty.arg);
+new_node->n = data;
+new_node->next = NULL;
+new_node->prev = NULL;
 
-	if (!(*stack))
-	{
-		(*stack) = new_node;
-		return;
-	}
-	new_node->next = (*stack);
-	(*stack)->prev = new_node;
-	(*stack) = new_node;
+if (!(*stack))
+{
+(*stack) = new_node;
+return;
+}
+new_node->next = (*stack);
+(*stack)->prev = new_node;
+(*stack) = new_node;
 }
